5f43f27add385eda33f24c6b079d45c85fe40a6e
fadygouda <fadygouda123@gmail.com>
2020-08-23 16:12:04 -0700

    typo in index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 
-const server = require('./api/server.js');
+const server = require('./Api/server.js')
 
 const PORT = process.env.DB_ENV === 'testing' ? 4000 : 5000;
 server.listen(PORT, () => {
     console.log(`n== Server listening on port ${PORT} ==/n`);
 });
+
+
+
 
