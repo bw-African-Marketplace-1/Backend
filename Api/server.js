09ba2470195cb90106b04b5073c1140e7446ed8c
fadygouda <fadygouda123@gmail.com>
2020-08-25 18:19:34 -0700

    cors user

--- Api/server.js
+++ Api/server.js
@@ -9,6 +9,7 @@ const server = express();
 // server.use(helmet());
 // server.use(cors());
 server.use(express.json());
+server.use(cors());
 
 server.use('/api/auth', authRouter);
 server.use('/api/business', businessRouter)
